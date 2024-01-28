56c09682322795e6db820efb23207d115fb2c125
Amanda Chan <achan214@calpoly.edu>
2024-01-28 13:59:24 -0800

    feat: added GET function for ALL volunteer roles for one event.

---
+++ src/app/api/event/[slug]/role/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+import connectDB from '@database/db'
+import volunteerRoleSchema from '@database/volunteerRoleSchema'
+
+type IParams = {
+    params: {
+        slug: string
+    }
+}
+
+export async function GET(req: NextRequest, { params }: IParams) {
+    await connectDB() // function from db.ts
+		const { slug } = params 
+
+	   try {
+	        const volunteerRoles = await volunteerRoleSchema.findOne().orFail()
+	        return NextResponse.json(volunteerRoles)
+	    } catch (err) {
+	        return NextResponse.json('No volunteer roles found.', { status: 404 })
+	    }
+}
--- src/app/api/event/route.ts
+++ src/app/api/event/route.ts
@@ -2,15 +2,8 @@ import { NextRequest, NextResponse } from 'next/server'
 import connectDB from '@database/db'
 import eventSchema from '@database/eventSchema'
 
-type IParams = {
-    params: {
-        slug: string
-    }
-}
-
-export async function GET(req: NextRequest, { params }: IParams) {
+export async function GET(req: NextRequest) {
     await connectDB() // function from db.ts
-		// const { slug } = params // another destructure
 
 	   try {
 	        const event = await eventSchema.findOne().orFail()
