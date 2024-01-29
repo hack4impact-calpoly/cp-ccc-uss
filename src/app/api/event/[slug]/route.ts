3e6c849e75808a4abceeb3c8810e67b9f6131d2a
unknown <kaylatran2021@gmail.com>
2024-01-29 01:04:00 -0800

    feat: editing an event POST API

--- src/app/api/event/[slug]/route.ts
+++ src/app/api/event/[slug]/route.ts
@@ -29,3 +29,25 @@ export async function GET(req: NextRequest, { params }: IParams) {
     return NextResponse.json({ err: "Internal Server Error" }, { status: 500 });
   }
 }
+
+/**
+ * PUT API for editing an event 
+ * @returns None
+ */
+export async function PUT(req: NextRequest, { params }: IParams) {
+  await connectDB();
+  const { slug } = params;
+
+  try {
+    const event = req.body;
+    if (event) {
+      const updatedEvent = await eventSchema.findByIdAndUpdate({ slug }, event);
+      return NextResponse.json(updatedEvent, { status: 201 });
+    } else {
+      return NextResponse.json("Invalid request body", { status: 400 });
+    }
+  } catch (error) {
+    console.error("Error creating event:", error);
+    return NextResponse.json("Internal Server Error", { status: 500 });
+  }
+}
