0cbebd3474aeceb9dafad0d87cdf5734064fd5e1
IvanA673 <22ivana04@gmail.com>
2024-01-28 20:27:01 -0800

    fix: tested GET request and changed folder structure to get volunteerRoleSchemas based on id.

--- src/app/api/role/[_id]/route.ts
+++ src/app/api/role/[_id]/route.ts
@@ -2,20 +2,20 @@ import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@database/db';
 import volunteerRoleSchema from '@database/volunteerRoleSchema';
 
-type IParams = {
+type IParams = { 
 		params: {
-			id: string
+			_id: string
 		}
 }
 
 export async function GET(req: NextRequest, { params }: IParams) {
     await connectDB()
-		const { id } = params //destructure
-
+		const { _id } = params //destructure
 	   try {
-	        const volunteerRole = await volunteerRoleSchema.findOne({ id }).orFail()
+	        const volunteerRole = await volunteerRoleSchema.findOne({ _id }).orFail()
 	        return NextResponse.json(volunteerRole)
 	    } catch (err) {
+            console.log(err)
 	        return NextResponse.json('Could not find the Volunteer Role.', { status: 404 })
 	    }
 }
