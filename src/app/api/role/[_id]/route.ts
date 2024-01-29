c716fb8deae15cd8dbed75a3f1fddbe57aa83442
Grace Boye-Williams <gboyewilliams@gmail.com>
2024-01-29 14:25:56 -0800

    feat: updated delete to remove role from events array

--- src/app/api/role/[_id]/route.ts
+++ src/app/api/role/[_id]/route.ts
@@ -1,10 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@database/db";
 import volunteerRoleSchema from "@database/volunteerRoleSchema";
+import Events from "@database/eventSchema";
 
 type IParams = {
   params: {
     _id: string;
+    event: string;
   };
 };
 
@@ -25,16 +27,33 @@ export async function GET(req: NextRequest, { params }: IParams) {
 export async function DELETE(req: NextRequest, { params }: IParams) {
   await connectDB();
   const { _id } = params; //destructure
+  const { event } = params;
   try {
-    const volunteerRole = await volunteerRoleSchema
+    const eventObj = await Events.findOne({ _id: event });
+
+    if (!eventObj) {
+      return NextResponse.json("Event not found.", { status: 404 });
+    }
+    const eventid = eventObj._id;
+
+    await Events.findByIdAndUpdate(
+      eventid,
+      { $pull: { roles: { _id }.toString() } },
+      { new: true }
+    );
+
+    const deletedVolunteerRole = await volunteerRoleSchema
       .findOneAndDelete({ _id })
       .orFail();
-    return NextResponse.json(volunteerRole);
+    if (!deletedVolunteerRole) {
+      return NextResponse.json("Could not find the Volunteer Role.", {
+        status: 404,
+      });
+    }
+    return NextResponse.json(deletedVolunteerRole);
   } catch (err) {
     console.log(err);
-    return NextResponse.json("Could not find the Volunteer Role.", {
-      status: 404,
-    });
+    return NextResponse.json("An error occurred.", { status: 500 });
   }
 }
 
