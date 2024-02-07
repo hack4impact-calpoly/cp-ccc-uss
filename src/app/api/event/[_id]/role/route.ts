3806ca114c48cc157308e13c161b6f8a9ce630ea
andrewtchan <andrew2020chan@gmail.com>
2024-02-06 16:38:18 -0800

    style: indentation

--- src/app/api/event/[_id]/role/route.ts
+++ src/app/api/event/[_id]/role/route.ts
@@ -1,21 +1,21 @@
-import { NextRequest, NextResponse } from 'next/server'
-import connectDB from '@database/db'
-import volunteerRoleSchema from '@database/volunteerRoleSchema'
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@database/db";
+import volunteerRoleSchema from "@database/volunteerRoleSchema";
 
 type IParams = {
-    params: {
-        slug: string
-    }
-}
+  params: {
+    slug: string;
+  };
+};
 
 export async function GET(req: NextRequest, { params }: IParams) {
-    await connectDB() // function from db.ts
-		const { slug } = params 
+  await connectDB(); // function from db.ts
+  const { slug } = params;
 
-	   try {
-	        const volunteerRoles = await volunteerRoleSchema.find().orFail()
-	        return NextResponse.json(volunteerRoles)
-	    } catch (err) {
-	        return NextResponse.json('No volunteer roles found.', { status: 404 })
-	    }
+  try {
+    const volunteerRoles = await volunteerRoleSchema.find().orFail();
+    return NextResponse.json(volunteerRoles);
+  } catch (err) {
+    return NextResponse.json("No volunteer roles found.", { status: 404 });
+  }
 }
