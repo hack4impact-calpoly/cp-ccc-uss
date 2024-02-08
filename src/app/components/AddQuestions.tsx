834088e8b179cf350d74e8c7842db19f01c2ecb9
Grace Boye-Williams <gboyewilliams@gmail.com>
2024-02-07 17:32:02 -0800

    feat progress on AddQuestions component

--- src/app/components/AddQuestions.tsx
+++ src/app/components/AddQuestions.tsx
@@ -1,1 +1,6 @@
-"use client";
+import React from 'react';
+import type { IVolunteerForm } from '@database/volunteerFormSchema'; 
+
+export default function AddQuestions(props: IVolunteerForm) {
+    return(<div></div>);
+}
