7e94f94046121d193b26cdfafd4a8cfe1cbc643a
Xavier Garcia <xmangarcia17@gmail.com>
2024-02-09 01:02:47 -0800

    feat: create POST endpoint in api/volunteer to create a volunteer

---
+++ src/app/api/volunteer/route.ts
@@ -0,0 +1,35 @@
+import connectDB from "@database/db";
+import { NextRequest, NextResponse } from "next/server";
+import volunteerSchema, { IVolunteer } from "@database/volunteerSchema";
+
+// create a Volunteer
+export async function POST(req: NextRequest) {
+  await connectDB();
+
+  try {
+    const { name, email, languages, roles, entries }: IVolunteer =
+      await req.json(); // collect volunteer data from req
+
+    if (!name || !email || !languages || !roles || !entries) {
+      console.error("Invalid request body - Missing required fields");
+      return NextResponse.json(
+        "Invalid request body - Missing required fields",
+        { status: 400 }
+      );
+    }
+    const volunteer = { name, email, languages, roles, entries };
+
+    try {
+      const createdVolunteer = await volunteerSchema.create(volunteer); // try to create volunteer in database
+      return NextResponse.json(createdVolunteer, { status: 201 });
+    } catch (error) {
+      console.error("Error creating volunteer:", error);
+      return NextResponse.json("Internal server error", { status: 500 });
+    }
+  } catch (error) {
+    console.error("Error parsing volunteer request body:", error);
+    return NextResponse.json("Invalid request body - Malformed JSON", {
+      status: 400,
+    });
+  }
+}
