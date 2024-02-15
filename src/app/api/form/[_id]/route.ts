9a6ad2f2d7d9aa13277d94eddc8ad0a2a8a377db
unknown <kaylatran2021@gmail.com>
2024-02-15 01:24:46 -0800

    feat: GET API for retrieving one volunteerForm by its id

--- src/app/api/form/[_id]/route.ts
+++ src/app/api/form/[_id]/route.ts
@@ -1,2 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@database/db";
+import volunteerFormSchema from "@database/volunteerFormSchema";
+
+type IParams = {
+  params: {
+    _id: string;
+  };
+};
+
+/**
+ * GET API for retrieving a volunteerForm by its id
+ * @returns None
+ */
+export async function GET(req: NextRequest, { params }: IParams) {
+  await connectDB();
+  const { _id } = params;
+
+  try {
+    const event = await volunteerFormSchema.findOne({ _id }).orFail();
+    return NextResponse.json(event, { status: 201 });
+  } catch (err: any) {
+    console.error("Error", err);
+
+    if (err.name === "DocumentNotFoundError") {
+      return NextResponse.json("No event found.", { status: 404 });
+    }
+
+    return NextResponse.json({ err: "Internal Server Error" }, { status: 500 });
+  }
+}
