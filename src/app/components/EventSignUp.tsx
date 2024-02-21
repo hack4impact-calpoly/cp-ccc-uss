8e2c0b7d30675d65b656006e56917533faf9bc70
Charles Moreno <93685786+charlesmoreno32@users.noreply.github.com>
2024-02-21 15:41:52 -0800

    feat: inital EventSignUp component setup

---
+++ src/app/components/EventSignUp.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import type { IEvent } from "../../database/eventSchema";
+import style from "@styles/EventSignUp.module.css";
+
+type IParams = {
+  id: string;
+};
+
+export default function EventSignUp({ id }: IParams) {
+  const [event, setEvent] = useState<IEvent | null>(null);
+
+  
+
+  return (
+    <div>EventSignUp</div>
+  );
+}
