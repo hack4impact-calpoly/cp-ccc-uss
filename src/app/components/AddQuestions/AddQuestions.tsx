8aa8ecf256f143b43e2e2732f9ef81cd40cf0757
Brandon <ebdon101@gmail.com>
2024-02-22 09:15:06 -0800

    feat: created input questions, question selector, and add options

--- src/app/components/AddQuestions/AddQuestions.tsx
+++ src/app/components/AddQuestions/AddQuestions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import type { IVolunteerForm } from '@database/volunteerFormSchema'; 
 
 import { AddCircle } from '@mui/icons-material';
@@ -7,34 +7,86 @@ import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+
 
 export default function AddQuestions(props: IVolunteerForm) {
-    const [age, setAge] = React.useState('');
+    const [inputValue, setInputValue] = useState('');
+    const [fieldType, setFieldType] = useState('Multiple Choice');
+    const [options, setOptions] = useState<string[]>([]);
+    const [currentOption, setCurrentOption] = useState('');
+    const [finalized, setFinalized] = useState(false);
+    const [clickableOption, setClickableOption] = useState('');
 
-    const handleChange = (event: SelectChangeEvent) => {
-      setAge(event.target.value as string);
+
+
+    const handleChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+        setInputValue(e.target.value);
+      };
+      
+    const handleOptionChange = (e: { target: { value: any; }; }) => {
+    setCurrentOption(e.target.value);
+    };    
+
+    const handleFieldTypeChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+        setFieldType(e.target.value);
+      };
+      
+    const handleAddOption = () => {
+
+        setOptions((options) => [...options, currentOption]);
+        setCurrentOption('');
+    
     };
+
+    const handleCircleClick = () => {
+        if (finalized) {
+
+        setOptions((options) => [...options, clickableOption]);
+        setCurrentOption('');
+
+        }
+    }
+    
     return(
     <div>
-        <Button variant="outlined" startIcon={<AddCircle />}></Button>
+         {//<Button variant="outlined" startIcon={<AddCircle />}></Button>
+            }
 
-        <Box sx={{ maxWidth: 200 }}>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Question</InputLabel>
-                <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={age}
-                label="Age"
-                onChange={handleChange}
-                >
-                <MenuItem value={"Short Answer"}>Short Answer</MenuItem>
-                <MenuItem value={"Multiple Choice"}>Multiple Choice</MenuItem>
+        <input 
+            type="text" 
+            value={inputValue}
+            placeholder="Enter Question"
+            onChange={handleChange}
+        />
+        <select value={fieldType} onChange={handleFieldTypeChange}>
+            <option value="Multiple Choice">Multiple Choice</option>
+            <option value="Short Answer">Short Answer</option>
+        </select>
 
-                </Select>
-            </FormControl>
-        </Box>
+        <div>
+            {fieldType == "Multiple Choice" ?         
+            <div>
+                <input
+                    type="text"
+                    value={currentOption}
+                    onChange={handleOptionChange}
+                    placeholder="Type an option..."
+                />
+                <button onClick={handleAddOption}>Add Option</button>
+                <ul>
+                    {options.map((option, index) => (
+                    <li key={index}>
+                        {option}
+                        {//maybe check if finalized and if it is then we can add clickable circles
+                        }
+                    </li>
+                    ))}
+                </ul>
+            </div> : <div><h1>Short Answer</h1></div>}
+
+
+        </div>
     </div>
     );
 }
