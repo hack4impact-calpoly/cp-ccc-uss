2f10ceafaea59938845d2b351da1b83d3cf2d7c3
Charles Moreno <93685786+charlesmoreno32@users.noreply.github.com>
2024-02-29 15:38:02 -0800

    feat: Event selection, skeleton somewhat done

--- src/app/components/EventSignUp.tsx
+++ src/app/components/EventSignUp.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import type { IEvent } from "../../database/eventSchema";
-import { Input, Select } from '@chakra-ui/react'
+import { Input, Radio, RadioGroup, Select } from "@chakra-ui/react";
 import style from "@styles/EventSignUp.module.css";
 
 type IParams = {
@@ -11,12 +11,16 @@ export default function EventSignUp({ id }: IParams) {
   const [event, setEvent] = useState<IEvent | null>(null);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState<any[]>([]);
+  const [roles, setRoles] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  /* as soon as event state is changed, whole page reloads  */
+  /*When looking to hide then show, check if event has been set, if not th4en don't show. If ithas, then show rest */
 
   async function fetchEvents() {
     try {
       const response = await fetch(`http://localhost:3000/api/event`);
-
+      console.log(response);
       if (!response.ok) {
         throw new Error(`Failed to fetch events. Status: ${response.status}`);
       }
@@ -34,11 +38,40 @@ export default function EventSignUp({ id }: IParams) {
   }, []);
 
   return (
-    <div>EventSignUp
+    <>
+      {events.length > 0 ? (
         <div>
-        <Input placeholder='Name' onChange={e => setName(e.target.value)}/>
-        <Input placeholder='Email' onChange={e => setEmail(e.target.value)}/>
+          <h1>Event Sign Up</h1>
+          <Input
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className={style.inputLine}
+          />
+          <Input
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className={style.inputLine}
+          />
+          <Select placeholder="Select Event">
+            {events &&
+              events.map((event) => (
+                <option key={event._id} value={event._id}>
+                  {event.name}
+                </option>
+              ))}
+          </Select>
         </div>
-    </div>
+      ) : (
+        <div>
+          <h1>Event Sign Up</h1>
+          <h2>No Events found.</h2>
+        </div>
+      )}
+      {/* {event ? <div></div> : <div></div>} This is for roles
+      {role ? <div></div> : <div></div>} This is for shift
+      {event ? <div></div> : <div></div>} This is for questions */}
+    </>
   );
 }
