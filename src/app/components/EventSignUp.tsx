f25bebbaaf003332c83f76d9c32f9ba108932cd3
unknown <kaylatran2021@gmail.com>
2024-03-04 02:11:04 -0800

    feat: shift options appear after role selected & multiple role/shift selection possible

--- src/app/components/EventSignUp.tsx
+++ src/app/components/EventSignUp.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import type { IEvent } from "../../database/eventSchema";
-import type { IVolunteerRole } from "../../database/volunteerRoleSchema";
+import type {
+  IVolunteerRole,
+  IVolunteerRoleTimeslot,
+} from "../../database/volunteerRoleSchema";
 import { Input, Select } from "@chakra-ui/react";
 import style from "@styles/EventSignUp.module.css";
 
@@ -14,7 +17,11 @@ export default function EventSignUp({ id }: IParams) {
   const [email, setEmail] = useState("");
   const [events, setEvents] = useState<IEvent[]>([]);
   const [roles, setRoles] = useState<IVolunteerRole[]>([]);
-  const [role, setRole] = useState<IVolunteerRole | null>(null);
+  const [selectedRoles, setSelectedRoles] = useState<IVolunteerRole[]>([]);
+  const [shifts, setShifts] = useState<IVolunteerRoleTimeslot[]>([]);
+  const [selectedShifts, setSelectedShifts] = useState<{
+    [roleId: string]: IVolunteerRoleTimeslot[];
+  }>({});
   const [questions, setQuestions] = useState([]);
 
   /* as soon as event state is changed, whole page reloads  */
@@ -85,6 +92,35 @@ export default function EventSignUp({ id }: IParams) {
     }
   }
 
+  async function handleRoleSelect(roleID: string) {
+    try {
+      const selectedRole: IVolunteerRole | undefined = roles.find(
+        (role) => role?._id === roleID
+      );
+
+      if (selectedRole && !selectedRoles.some((role) => role._id === roleID)) {
+        setSelectedRoles([...selectedRoles, selectedRole]); // Add selected role to the array if it doesn't already exist
+        setShifts(selectedRole.timeslots);
+      }
+    } catch (error) {
+      console.error("Error fetching shifts:", error);
+      setShifts([]);
+    }
+  }
+  function handleShiftSelect(shift: IVolunteerRoleTimeslot) {
+    setSelectedShifts((prevSelectedShifts) => ({
+      ...prevSelectedShifts,
+      [selectedRoles[selectedRoles.length - 1]._id]: prevSelectedShifts[
+        selectedRoles[selectedRoles.length - 1]._id
+      ]
+        ? [
+            ...prevSelectedShifts[selectedRoles[selectedRoles.length - 1]._id],
+            shift,
+          ]
+        : [shift],
+    }));
+  }
+
   useEffect(() => {
     fetchEvents();
   }, []);
@@ -131,7 +167,14 @@ export default function EventSignUp({ id }: IParams) {
       )}
       {event ? ( //Do not show this section until there is an event picked
         <div>
-          <Select variant="filled" placeholder="Select Role">
+          <Select
+            variant="filled"
+            placeholder="Select Role"
+            onChange={(e) => {
+              const selectedRoleID = e.target.value;
+              handleRoleSelect(selectedRoleID);
+            }}
+          >
             {roles.length > 0 &&
               roles.map((role) => (
                 <option key={role._id} value={role._id}>
@@ -143,6 +186,26 @@ export default function EventSignUp({ id }: IParams) {
       ) : (
         <div></div>
       )}
+      {selectedRoles.map((role) => (
+        <div key={role._id}>
+          <h3>Select Shifts for {role.roleName}:</h3>
+          {role.timeslots.map((shift, index) => (
+            <div key={index}>
+              <input
+                type="checkbox"
+                id={`shift-${index}`}
+                value={`shift-${index}`}
+                onChange={() => handleShiftSelect(shift)}
+                checked={selectedShifts[role._id]?.includes(shift)}
+              />
+              <label htmlFor={`shift-${index}`}>
+                {`Shift ${index + 1}: ${shift.startTime} - ${shift.endTime}`}
+              </label>
+            </div>
+          ))}
+        </div>
+      ))}
+
       {/* {role ? <div></div> : <div></div>} This is for shifts, //Do not show this section until there is a role picked
         {event ? <div></div> : <div></div>} This is for questions, //Do not show this section until there is an event picked */}
     </>
