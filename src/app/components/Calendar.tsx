920d61bd6d58d452f401d7caa0674fd538f9b459
Grace Boye-Williams <gboyewilliams@gmail.com>
2024-03-05 15:02:42 -0800

    feat: added logic for admin true/false

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -1,14 +1,23 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import dayGridPlugin from '@fullcalendar/daygrid';
+import dayGridPlugin from "@fullcalendar/daygrid";
 import type { IEvent } from "@database/eventSchema";
-import Link from 'next/link';
+import Link from "next/link";
 import { useRef } from "react";
 import { EventClickArg } from "@fullcalendar/core/index.js";
 import { EventInstance } from "@fullcalendar/core/internal";
-import {Modal,ModalOverlay,ModalContent,ModalHeader,ModalFooter,ModalBody,ModalCloseButton, Button} from '@chakra-ui/react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from "@chakra-ui/react";
 import UserEventDetails from "./UserEventDetails";
 
 //Interface to define full calendar event format
@@ -18,55 +27,55 @@ interface FullCalendarEvent {
   start: Date;
 }
 
-
-const Calendar = ({admin = false}) => {
+const Calendar = ({ admin = false }) => {
   const [events, setEvents] = useState<IEvent[]>([]);
-  const [fullCalendarEvents, setFullCalendarEvents] = useState<FullCalendarEvent[]>([]);
-  const [selectedEventId, setSelectedEventId] = useState('');
+  const [fullCalendarEvents, setFullCalendarEvents] = useState<
+    FullCalendarEvent[]
+  >([]);
+  const [selectedEventId, setSelectedEventId] = useState("");
   const [detailModalOpen, setDetailModalOpen] = useState(false);
 
   //get all events on first render
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await fetch('/api/event/');
-        const eventsFromDB = await response.json(); 
+        const response = await fetch("/api/event/");
+        const eventsFromDB = await response.json();
         setEvents(eventsFromDB);
-
       } catch (error) {
-        console.error('Error fetching events:', error);
+        console.error("Error fetching events:", error);
       }
     };
 
     fetchEvents();
   }, []);
-  
+
   //updates state varibales
   const handleEventClick = (info: EventClickArg) => {
     setSelectedEventId(info.event.id);
     setDetailModalOpen(true);
-    console.log(detailModalOpen)
+    console.log(detailModalOpen);
   };
-  
+
   const handleCloseModal = () => {
     setDetailModalOpen(false);
-  }
+  };
 
   // useEffect to convert events to FullCalendar compatible events whenever events array changes
   useEffect(() => {
     const convertEventsToFCFormat = () => {
-      if (events !== null){
-        const FullCalendarEvents = events.map(event => ({
+      if (events !== null) {
+        const FullCalendarEvents = events.map((event) => ({
           id: event._id,
           title: event.name,
-          start: event.date //start is the date field for the full calendar 
-        }))
+          start: event.date, //start is the date field for the full calendar
+        }));
         setFullCalendarEvents(FullCalendarEvents);
       }
-    }
+    };
 
     convertEventsToFCFormat();
-  }, [events])
+  }, [events]);
 
   return (
     <>
@@ -74,26 +83,33 @@ const Calendar = ({admin = false}) => {
         plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
         editable
         selectable
-        initialView='dayGridMonth'
-        events = {fullCalendarEvents}
+        initialView="dayGridMonth"
+        events={fullCalendarEvents}
         eventClick={handleEventClick}
       />
 
       <Modal size="xl" isOpen={detailModalOpen} onClose={handleCloseModal}>
-      <ModalOverlay />
-      <ModalContent >
-        <ModalBody>
-          <UserEventDetails id={selectedEventId} />
-        </ModalBody>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalBody>
+            {admin ? (
+              <div>
+                <p>Admin Section:</p>
+                <UserEventDetails id={selectedEventId} />
+              </div>
+            ) : (
+              <UserEventDetails id={selectedEventId} />
+            )}
+          </ModalBody>
 
-        <ModalFooter>
-          <Button colorScheme='blue' mr={3} onClick={handleCloseModal}>
-            Close
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-  </>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleCloseModal}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
