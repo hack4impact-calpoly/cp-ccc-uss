b7391c62f89b4ebc0dc08cd07db61d90cd457528
andrewtchan <andrew2020chan@gmail.com>
2024-03-06 19:26:45 -0800

    chore: remove logging statement

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -6,7 +6,6 @@ import Google from "next-auth/providers/google";
 
 export default function Login() {
     const { data: session } = useSession();
-    console.log(session);
     if(session) { //if user logged in, show signout
         return (
           <>
