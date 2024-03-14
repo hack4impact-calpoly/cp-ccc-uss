51224c7375cede58d1786c17f35956c4f4eb1ecb
unknown <kaylatran2021@gmail.com>
2024-03-14 02:18:42 -0700

    refactor: commenting out code that I think not necessary* should check this

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <Calendar />
       <CreateEvent />
+      {/* <Calendar /> */}
       <div
         style={{
           display: "flex",
