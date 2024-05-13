ed9de387cb55fb74d35c56e1f6609b096f2a8e74
IvanA673 <22ivana04@gmail.com>
2024-05-12 22:32:23 -0700

    feat: user authenicated for admin on home page and admin can switch views with a button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,23 @@ import React, { useState } from 'react';
 import Navbar from "@components/Navbar";
 import Calendar from "@components/Calendar";
 import CreateEvent from '@components/CreateEvent/CreateEvent';
+import { useUser } from '@clerk/nextjs';
+import { Button } from '@chakra-ui/react';
+
 export default function Home() {
   const [admin, setAdmin] = useState(false);
-  
+  let adminbutton = false;
+
+  const { isLoaded, isSignedIn, user } = useUser();
+  const admins = process.env.NEXT_PUBLIC_ADMIN_EMAIL_ADDRESSES?.split(",");
+  if (user && isSignedIn && isLoaded && user.primaryEmailAddress) {
+    if (admins?.includes(user.primaryEmailAddress.emailAddress)) {
+      adminbutton = true;
+    } else {
+      adminbutton = false;
+      setAdmin(false);
+    }
+  }
   return (
     <main>
       <div
@@ -17,10 +31,15 @@ export default function Home() {
         }}
       >
         <Navbar />
-        <div>i am the admin: {admin.toString()}</div>
-        <button onClick={() => (admin ? setAdmin(false) : setAdmin(true))}>
-          secure button
-        </button>
+        <div>
+          {adminbutton ? (
+            <div>
+              <Button onClick={() => (admin ? setAdmin(false) : setAdmin(true))} mt={3} p={6} colorScheme="teal">
+                {admin ? <>Admin <br/> Change View</> : <>User <br/> Change View</>}
+              </Button>
+            </div>) :
+            null}
+        </div>
         <div className="h-screen">
         </div>
         <div style={{ width: "70%", margin: "20px" }}>
