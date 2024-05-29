cf8f5a032589119c90c8391bcdf0a4b9468cf8a2
andrewtchan <andrew2020chan@gmail.com>
2024-05-28 21:49:30 -0700

    feat: conditionally render signup button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,21 +12,25 @@ import { Button } from '@chakra-ui/react';
 export default function Home() {
   const [admin, setAdmin] = useState(false);
   const [adminbutton, setAdminButton] = useState(false);
+  const [signUpButton, setSignUpButton] = useState(false);
 
   const { isLoaded, isSignedIn, user } = useUser();
 
   useEffect(() => {
     if (user) {
       const orgs = user.organizationMemberships;
-      if (orgs?.some(org => org.organization.name === "CCC-USS-Admins")) {
+      if (orgs?.some(org => org.organization.name === "CCC-USS-Admins")) { // admin
         setAdminButton(true);
-      } else {
+        setSignUpButton(false);
+      } else { // normal volunteer 
         setAdminButton(false);
         setAdmin(false);
+        setSignUpButton(true);
       }
-    } else {
+    } else { // not signed in
       setAdminButton(false);
       setAdmin(false);
+      setSignUpButton(false);
     }
   },[user, isSignedIn, isLoaded]);
 
@@ -50,9 +54,7 @@ export default function Home() {
             </div>) :
             null}
         </div>
-        <EventSignUp />
-        <div className="h-screen">
-        </div>
+        {signUpButton || (adminbutton && !admin) ? <EventSignUp /> : null}
         <div style={{ width: "70%", margin: "20px" }}>
           <Calendar admin={admin} />
         </div>
